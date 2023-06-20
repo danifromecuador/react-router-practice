a0388a51fe481842d22a36f6e3b6db45fa129ada
dani <danifromecuador@gmail.com>
2023-06-20 12:29:40 -0500

    Create a Navbar component with 3 links

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => (
+  <nav>
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/contact">Contact</a></li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
